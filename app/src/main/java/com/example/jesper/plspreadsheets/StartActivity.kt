908e4d5c6eaff0d7fbc5e422025bcc8a235231f9
Jesper Bergström <jesper.b96@gmail.com>
2017-08-22 16:02:52 +0200

    Added getting spreadsheets from assets

--- app/src/main/java/com/example/jesper/plspreadsheets/StartActivity.kt
+++ app/src/main/java/com/example/jesper/plspreadsheets/StartActivity.kt
@@ -8,5 +8,13 @@ class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        getSpreadsheets()
+    }
+
+    private fun getSpreadsheets(){
+        var files = assets
+        val filelist = files.list("")
+        println(filelist[1])
+        println(filelist[2])
     }
 }
