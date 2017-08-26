19ad08c89156f18f8d37b9efbaa19825a117d652
Jesper Bergström <jesper.b96@gmail.com>
2017-08-26 22:32:46 +0200

    Implemented a ListView that displays all spreadsheets

--- app/src/main/java/com/example/jesper/plspreadsheets/StartActivity.kt
+++ app/src/main/java/com/example/jesper/plspreadsheets/StartActivity.kt
@@ -2,19 +2,48 @@ package com.example.jesper.plspreadsheets
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import com.example.jesper.plspreadsheets.R.layout
+import java.util.*
 
+/**
+ * Start screen for the app. Here, the user can either create a
+ * new spreadsheet or select an already saved spreadsheet to view.
+ *
+ * @author Jesper Bergstrom
+ * @name StartActivity
+ * @version 0.00.00
+ */
 class StartActivity : AppCompatActivity() {
+
+    private var adapter: ArrayAdapter<String>? = null
+    private var listItems = ArrayList<String>()
+    private var listView: ListView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        setContentView(layout.activity_start)
+        adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems)
+        listView = findViewById(R.id.listView) as ListView
+        (listView as ListView).adapter = adapter
         getSpreadsheets()
     }
 
+    /**
+     * Reads from the spreadsheets folder and puts all spreadsheets in
+     * a list that will be displayed in the ListView.
+     */
     private fun getSpreadsheets(){
-        var files = assets
-        val filelist = files.list("")
-        println(filelist[1])
-        println(filelist[2])
+        listItems.clear()
+        val files = assets
+        val filelist = files.list("spreadsheets")
+        var i = 0
+        while(i < filelist.size){
+            println(filelist[i])
+            listItems.add(filelist[i])
+            i++
+        }
     }
 }
+
